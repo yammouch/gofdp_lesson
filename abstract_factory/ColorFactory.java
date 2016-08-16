90db2fb73d62ede36f91b105dd5ef901979034c5
U-OFFICE\tyam <tyam@N34125.office.amsiag.com>
2016-08-16 09:55:24 +0900

    First checkin.

---
+++ abstract_factory/ColorFactory.java
@@ -0,0 +1,21 @@
+public class ColorFactory extends AbstractFactory {
+  @Override
+  Shape getShape(String shape) {
+    return null;
+  }
+
+  @Override
+  public Color getColor(String colorType) {
+    if (colorType == null) {
+      return null;
+    }
+    if (colorType.equalsIgnoreCase("RED")) {
+      return new Red();
+    } else if (colorType.equalsIgnoreCase("GREEN")) {
+      return new Green();
+    } else if (colorType.equalsIgnoreCase("BLUE")) {
+      return new Blue();
+    }
+    return null;
+  }
+}
